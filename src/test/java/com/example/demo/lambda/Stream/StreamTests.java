f788c50f539127b4ae24ca23f5823a37b305d48a
Vadym <vadym.snigur.code@gmail.com>
2021-10-07 17:50:37 +0300

    added more samples about iterations

--- src/test/java/com/example/demo/lambda/Stream/StreamTests.java
+++ src/test/java/com/example/demo/lambda/Stream/StreamTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -17,11 +18,11 @@ public class StreamTests {
 
         System.out.println(Stream.empty());
         Stream.of("one");
-        Stream.of("one","two","three");
+        Stream.of("one", "two", "three");
 
-        Stream.generate(()->"one");
+        Stream.generate(() -> "one");
 
-        Stream.iterate("+", s->s+"+");
+        Stream.iterate("+", s -> s + "+");
 
         ThreadLocalRandom.current().ints();
         IntStream stream1 = "hello".chars();
@@ -38,8 +39,8 @@ public class StreamTests {
     public void testPrintPersons() {
         List<Person> persons = new ArrayList<>();
         persons.stream()                // Stream<Person>
-                .map(p->p.getAge())     // Stream<Integer>
-                .filter(age->age>20)    // Stream<Integer>
+                .map(p -> p.getAge())     // Stream<Integer>
+                .filter(age -> age > 20)    // Stream<Integer>
                 .forEach(System.out::println);
 
 //        persons.stream()                // Stream<Person>
@@ -53,28 +54,67 @@ public class StreamTests {
 //                .filter(age->age>20)    // Stream<Integer>
 //                .forEach(System.out::println);
 
-                persons.stream()                // Stream<Person>
-                .map(p->p.getAge())     // Stream<Integer>
+        persons.stream()                // Stream<Person>
+                .map(p -> p.getAge())     // Stream<Integer>
                 .peek(System.out::println)  //DOES NOT COMPILE
-                .filter(age->age>20)    // Stream<Integer>
+                .filter(age -> age > 20)    // Stream<Integer>
                 .forEach(System.out::println);
     }
 
     @Test
     public void testSkipLimitTesting() {
-        Person p1 = new Person("Alice",23);
-        Person p2 = new Person("Brian",56);
-        Person p3 = new Person("Chelsea",15);
-        Person p4 = new Person("David",28);
-        Person p5 = new Person("Erica",37);
-        Person p6 = new Person("Francisco",18);
+        Person p1 = new Person("Alice", 23);
+        Person p2 = new Person("Brian", 56);
+        Person p3 = new Person("Chelsea", 15);
+        Person p4 = new Person("David", 28);
+        Person p5 = new Person("Erica", 37);
+        Person p6 = new Person("Francisco", 18);
 
-        List<Person> persons = Arrays.asList(p1,p2,p3,p4,p5,p6);
+        List<Person> persons = Arrays.asList(p1, p2, p3, p4, p5, p6);
         persons.stream()
                 .skip(2)
                 .limit(3)
-                .filter(person -> person.getAge()>20)
+                .filter(person -> person.getAge() > 20)
                 .forEach(System.out::println);
 
+//        Optional<Person> opt = persons
+//                .stream()
+//                .findAny((Person) p -> p.getAge() > 20 );
+
     }
+
+    @Test
+    public void testSimpleStreams() {
+        List<Integer> ints = Arrays.asList(0, 1, 2, 3, 4);
+        Stream<Integer> stream1 = ints.stream();
+        stream1.forEach(System.out::println);
+
+        Stream<Integer> stream = Stream.of(0, 1, 2, 3, 4);
+        stream.forEach(System.out::println);
+
+        Stream<String> stringStream = Stream.generate(() -> "one");
+        stringStream.limit(5).forEach(System.out::println);
+
+
+    }
+
+    @Test
+    public void testSimpleStreamsLimit() {
+        Stream<String> stringStream = Stream.generate(() -> "one");
+        stringStream.limit(5).forEach(System.out::println);
+    }
+
+    @Test
+    public void testSimpleStreamsIterate() {
+        Stream<String> streamOfString = Stream
+                .iterate("+", s -> s + "+");
+        streamOfString.limit(5).forEach(System.out::println);
+    }
+
+    @Test
+    public void testSimpleStreamsThreadLocalRandom() {
+        IntStream streamOfInt = ThreadLocalRandom.current().ints();
+        streamOfInt.limit(5).forEach(System.out::println);
+    }
+
 }
