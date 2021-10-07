845ee7c2c07922c1a268315bd71add1764681dcd
Vadym <vadym.snigur.code@gmail.com>
2021-10-07 19:13:46 +0300

    added arrays problems implementation

---
+++ src/test/java/com/example/demo/Generics/ArraysIssue/ArrayIssuesTest.java
@@ -0,0 +1,25 @@
+package com.example.demo.Generics.ArraysIssue;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+public class ArrayIssuesTest {
+    @Test
+    public void testArrayIssues() {
+        Person donDraper = new Person("Don Drapper", 89);
+        Person peggyOlson = new Person("Peggy Olson",65);
+        Person[] madMen = {donDraper, peggyOlson};
+        System.out.println(Arrays.toString(madMen));
+        Person bertCooper = new Person("Bert Coper", 100);
+        madMen = add(bertCooper, madMen);
+        System.out.println(Arrays.toString(madMen));
+    }
+
+    private Person[] add(final Person person, Person[] array) {
+        int length = array.length;
+        array = Arrays.copyOf(array, length+1);
+        array[length] = person;
+        return array;
+    }
+}
